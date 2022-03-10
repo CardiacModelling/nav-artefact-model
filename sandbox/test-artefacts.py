cd226ad917fa5648eb4109235d9fbb2565524885
Chon Lok Lei <chonloklei@gmail.com>
2022-03-10 14:19:56 +0800

    Slight update to match API

--- sandbox/test-artefacts.py
+++ sandbox/test-artefacts.py
@@ -25,7 +25,8 @@ x.set_artefact_parameters({
     'voltage_clamp.Cm_est': 10.,
     'voltage_clamp.R_series_est': 6e-3,
     'voltage_clamp.C_prs_est': 0,
-    'voltage_clamp.alpha_P': 0.7,
+    #'voltage_clamp.alpha_R': 0.7,
+    #'voltage_clamp.alpha_P': 0.7,
     'voltage_clamp.g_leak': 0.5,
     'voltage_clamp.g_leak_est': 0.4,
 })
@@ -39,6 +40,7 @@ axes[1].plot(y.times(), y.simulate([.1] * y.n_parameters()), label='ideal')
 for i in range(1, 9):
     x.set_artefact_parameters({
         'voltage_clamp.alpha_R': i / 10.,
+        'voltage_clamp.alpha_P': i / 10.,
     })
     axes[0].plot(x.times(), x.voltage([.1] * x.n_parameters()))
     axes[1].plot(x.times(), x.simulate([.1] * x.n_parameters()), label=f'alpha={i/10.}')
